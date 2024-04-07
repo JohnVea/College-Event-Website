a6ffc67040403b77c8afff1af26154a934ce28a7
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-07 14:44:17 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -44,7 +44,8 @@ if (!$locId) {
     // Location doesn't exist, insert it into the Locations table
     //$locId = substr($location, 0, 10); // Truncate the name if it's too long
     $locId = $location;
-    $descr = substr($description, 0, 30); // Change this to your desired default description
+    //$descr = substr($description, 0, 30); // Change this to your desired default description
+    $descr = $description;
     $insertLocationStmt = $conn->prepare("INSERT INTO Locations (LocID, Name, Descr, Longitude, Latitude) VALUES (?, ?, ?, ?, ?)");
     $insertLocationStmt->bind_param("sssss", $locId, $location, $descr, $longitude, $latitude);
     if (!$insertLocationStmt->execute()) {
