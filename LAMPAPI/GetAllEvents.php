ed1616f368871c7fe9067f022d09fc5ed81a3ff8
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-07 17:21:18 -0400

    Update GetAllEvents.php

--- LAMPAPI/GetAllEvents.php
+++ LAMPAPI/GetAllEvents.php
@@ -1,52 +1,56 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Allow-Headers: Content-Type");
-  
-    header("Content-Type: application/json");
-    $conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
-    
-    if ($conn->connect_error) { 
-        returnWithError($conn->connect_error);
-    } else {
-        $resultArray = array(); // Initialize an array to store event data
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-        $sql = "SELECT * FROM Events";
-        $result = $conn->query($sql);
+header("Content-Type: application/json");
+$conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
 
-        if ($result && $result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $EventID = $row['Events_ID'];
-                $Time = $row['Time'];
-                $Location = $row['Location'];
-                $EventName = $row['Event_name'];
-                $Description = $row['Description'];
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $resultArray = array(); // Initialize an array to store event data
 
-                $event = array(
-                    "EventID" => $EventID,
-                    "Time" => $Time,
-                    "Location" => $Location,
-                    "EventName" => $EventName,
-                    "Description" => $Description
-                );
-                array_push($resultArray, $event);
-            }
+    $sql = "SELECT Events_ID, DATE(Time) as EventDate, TIME(Time) as EventTime, Location, Event_name, Description FROM Events";
+    $result = $conn->query($sql);
 
-            sendResultInfoAsJson($resultArray);
-        } else {
-            returnWithError("No Records Found");
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $EventID = $row['Events_ID'];
+            $EventDate = $row['EventDate'];
+            $EventTime = $row['EventTime'];
+            $Location = $row['Location'];
+            $EventName = $row['Event_name'];
+            $Description = $row['Description'];
+
+            $event = array(
+                "EventID" => $EventID,
+                "EventDate" => $EventDate,
+                "EventTime" => $EventTime,
+                "Location" => $Location,
+                "EventName" => $EventName,
+                "Description" => $Description
+            );
+            array_push($resultArray, $event);
         }
 
-        $conn->close();
+        sendResultInfoAsJson($resultArray);
+    } else {
+        returnWithError("No Records Found");
     }
-    
-    function sendResultInfoAsJson($obj) {
-        header('Content-type: application/json');
-        echo json_encode($obj); // Encode the array as JSON before sending
-    }
-    
-    function returnWithError($err) {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
-    }
+
+    $conn->close();
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj); // Encode the array as JSON before sending
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
 ?>
