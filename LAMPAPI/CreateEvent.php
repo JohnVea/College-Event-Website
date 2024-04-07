a065a1256e5ee55cb17f08acd051bd0523c82e15
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-07 12:31:36 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -1,58 +1,54 @@
 <?php
 
-// Allow requests from any origin
-header("Access-Control-Allow-Origin: *");
-
-// Allow certain methods (e.g., POST)
-header("Access-Control-Allow-Methods: POST");
-
-// Allow specific headers
-header("Access-Control-Allow-Headers: Content-Type");
-
-// Set response content type
-header("Content-Type: application/json");
-
-$inData = getRequestInfo();
-
-$time = $inData['time'];
-$location = $inData['location'];
-$eventName = $inData['eventName'];
-$description = $inData['description'];
-
-$conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
-
-if ($conn->connect_error) { 
-    returnWithError($conn->connect_error);
-} else {
-    $stmt = $conn->prepare("INSERT INTO Events (Time, Location, Event_name, Description) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $time, $location, $eventName, $description);
-
-    if ($stmt->execute()) {
-        $response = array("message" => "Event created successfully");
-        sendResultInfoAsJson($response);
+    // Allow requests from any origin
+    header("Access-Control-Allow-Origin: *");
+    
+    // Allow certain methods (e.g., POST)
+    header("Access-Control-Allow-Methods: POST");
+    
+    // Allow specific headers
+    header("Access-Control-Allow-Headers: Content-Type");
+    
+    // Set response content type
+    header("Content-Type: application/json");
+    
+    $inData = getRequestInfo();
+    
+    $time = $inData['time'];
+    $location = $inData['location'];
+    $eventName = $inData['eventName'];
+    $description = $inData['description'];
+    
+    $conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
+    
+    if ($conn->connect_error) { 
+        returnWithError($conn->connect_error);
     } else {
-        returnWithError("Failed to create event");
+        $stmt = $conn->prepare("INSERT INTO Events (Time, Location, Event_name, Description) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $time, $location, $eventName, $description);
+    
+        if ($stmt->execute()) {
+            $response = array("message" => "Event created successfully");
+            sendResultInfoAsJson($response);
+        } else {
+            returnWithError("Failed to create event");
+        }
+    
+        $stmt->close();
+        $conn->close();
     }
-
-    $stmt->close();
-    $conn->close();
-}
-
-function getRequestInfo() {
-    $data = $_POST;
-    if (empty($data)) {
-        // If $_POST is empty, try to get the data from the raw request body
-        $data = json_decode(file_get_contents('php://input'), true);
+    
+    function getRequestInfo() {
+        $json = file_get_contents('php://input');
+        return json_decode($json, true);
     }
-    return $data;
-}
-
-function sendResultInfoAsJson($obj) {
-    echo json_encode($obj);
-}
-
-function returnWithError($err) {
-    $response = array("error" => $err);
-    sendResultInfoAsJson($response);
-}
+    
+    function sendResultInfoAsJson($obj) {
+        echo json_encode($obj);
+    }
+    
+    function returnWithError($err) {
+        $response = array("error" => $err);
+        sendResultInfoAsJson($response);
+    }
 ?>
