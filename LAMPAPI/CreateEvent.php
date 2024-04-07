fd10036ad30030377169fed89d65ea599af18be7
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-07 11:59:59 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -1,34 +1,38 @@
 <?php
+    // Allow requests from any origin
     header("Access-Control-Allow-Origin: *");
+    
+    // Allow certain methods (e.g., POST)
     header("Access-Control-Allow-Methods: POST");
+    
+    // Allow specific headers
     header("Access-Control-Allow-Headers: Content-Type");
     
+    // Set response content type
     header("Content-Type: application/json");
+
+    $inData = getRequestInfo();
     $conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
     
     if ($conn->connect_error) { 
         returnWithError($conn->connect_error);
     } else {
-        // Get the JSON data from the request body
-        $json_data = file_get_contents('php://input');
-        
-        // Decode JSON data
-        $data = json_decode($json_data, true);
-        
-        // Extract data fields
-        $time = $data['time'];
-        $location = $data['location'];
-        $eventName = $data['eventName'];
-        $description = $data['description'];
+        // Retrieve event data from request
+        $time = $inData["time"];
+        $location = $inData["location"];
+        $eventName = $inData["eventName"];
+        $description = $inData["description"];
 
+        // Prepare and execute SQL statement to insert new event
         $stmt = $conn->prepare("INSERT INTO Events (Time, Location, Event_name, Description) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssss", $time, $location, $eventName, $description);
         
         if ($stmt->execute()) {
-            $eventId = $stmt->insert_id;
-            $response = array("eventId" => $eventId);
+            // If insertion is successful, return success response
+            $response = array("message" => "Event created successfully");
             sendResultInfoAsJson($response);
         } else {
+            // If there's an error, return error response
             returnWithError("Failed to create event");
         }
 
@@ -36,13 +40,16 @@
         $conn->close();
     }
     
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
     function sendResultInfoAsJson($obj) {
-        header('Content-type: application/json');
-        echo json_encode($obj); // Encode the array as JSON before sending
+        echo json_encode($obj);
     }
     
     function returnWithError($err) {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
+        $response = array("error" => $err);
+        sendResultInfoAsJson($response);
     }
 ?>
