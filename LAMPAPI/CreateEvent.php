2d8a51901eb4b927301804e0c757adf4cea73b4e
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-07 12:17:08 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -11,38 +11,42 @@
     // Set response content type
     header("Content-Type: application/json");
 
+
+    $inData = getRequestInfo();
+    $time = $data['time'];
+    $location = $data['location'];
+    $eventName = $data['eventName'];
+    $description = $data['description'];
+
     $conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
     
     if ($conn->connect_error) { 
         returnWithError($conn->connect_error);
     } else {
-        // Retrieve raw JSON data from request body
-        $json_data = file_get_contents('php://input');
-        
-        // Decode JSON data
-        $data = json_decode($json_data, true);
-        
-        // Extract data fields
-        $time = $data['time'];
-        $location = $data['location'];
-        $eventName = $data['eventName'];
-        $description = $data['description'];
-
-        // Prepare and execute SQL statement to insert new event
         $stmt = $conn->prepare("INSERT INTO Events (Time, Location, Event_name, Description) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssss", $time, $location, $eventName, $description);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
         
         if ($stmt->execute()) {
-            // If insertion is successful, return success response
-            $response = array("message" => "Event created successfully");
+            response = array("message" => "Event created successfully");
             sendResultInfoAsJson($response);
         } else {
-            // If there's an error, return error response
             returnWithError("Failed to create event");
         }
-
+    
         $stmt->close();
         $conn->close();
+    }
+    
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
     }
     
     function sendResultInfoAsJson($obj) {
@@ -53,4 +57,9 @@
         $response = array("error" => $err);
         sendResultInfoAsJson($response);
     }
+
+
+
+    
+    
 ?>
