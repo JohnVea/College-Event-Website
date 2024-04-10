e1551ca03e07223f60f3435fcd04aeaa068ee6f3
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-10 18:28:01 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -11,6 +11,9 @@
    
    // Set response content type
    header("Content-Type: application/json");
+   header("Access-Control-Allow-Credentials: true");
+   header("Access-Control-Max-Age: 3600");
+   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
    
    $inData = getRequestInfo();
    
