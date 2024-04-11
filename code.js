586b78ad9f67c3f3acff76e45d5a9b0936da3b0a
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-11 15:48:59 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -4,7 +4,61 @@ const searchBar = document.getElementById('searchBar');
 const cancelLoginButton = document.querySelector('.cancelLoginButton');
 const Login = document.querySelector('.userLoginButton');
 const SignedInUser = document.querySelector('.signedOnUser');
-const loginResult = document.getElementById('loginResult'); // Assuming element exists
+const loginResult = document.getElementById('loginResult');
+const registrationDiv = document.querySelector('.registration');
+const userRegisterButton = document.querySelector('.userRegisterButton');
+const cancelRegisterButton = document.querySelector('.cancelRegisterButton');
+const registerUserButton = document.querySelector('.registerUserButton');
+
+
+userRegisterButton.addEventListener('click', function(){
+    loginContainer.style.display = 'none';
+    registrationDiv.style.display = 'block';
+});
+
+cancelRegisterButton.addEventListener('click', function() {
+    registrationDiv.style.display = 'none';
+    loginContainer.style.display = 'block';
+});
+
+registerUserButton.addEventListener('click', function() {
+    doRegister();
+});
+
+async function doRegister() {
+    const username = document.getElementById("registerUsername").value;
+    const password = document.getElementById("registerPassword").value;
+    const email = document.getElementById("registerEmail").value;
+    const firstName = document.getElementById("registerFirstName").value;
+    const lastName = document.getElementById("registerLastName").value;
+    
+
+    try {
+        const response = await fetch('http://unieventverse.com/LAMPAPI/Register.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username : username, password : password, lirstName : firstName, LastName : lastName, email : email })
+        });
+
+        if (!response.ok) {
+            throw new Error('Registration failed');
+        }
+
+        const data = await response.json();
+
+        if (data.error === "") {
+            console.log('Registration successful:', data);
+            localStorage.setItem('userData', JSON.stringify(data));
+            window.location.href = "./Signed-In/home.html";
+        } else {
+            throw new Error(data.error);
+        }
+    } catch (error) {
+        console.error('Error during registration:', error.message);
+    }
+}
 
 loginButton.addEventListener('click', function() {
     console.log('Login button clicked');
