7814c89a4f0e41e4dba1ccaaf8eb818a4f530bf3
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-12 20:43:25 -0400

    Add files via upload

--- Signed-In/script.js
+++ Signed-In/script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const eventCardContainer = document.querySelector('.displayEventsContainer');
             const eventName = event.target.querySelector('h1');
             //const eventName = eventCard.querySelector('.eventName');
-            const eventDate = eventCard.querySelector('.eventDate').textContent;
+            const eventDate = eventCard.querySelector('.eventDate');
             const eventTime = eventCard.querySelector('h3');
             const longitude = eventCard.querySelector('h3').textContent;
             // const latitude = eventCard.querySelector('.latitude').textContent;
