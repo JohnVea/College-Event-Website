3bf62d93a18da77991c0f36a58b3049034c69c8e
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-12 22:16:35 -0400

    Add files via upload

--- Signed-In/script.js
+++ Signed-In/script.js
@@ -34,7 +34,8 @@ document.addEventListener("DOMContentLoaded", function() {
             
             const eventData = event.target.querySelectorAll('*'); // Select the event card
             console.log(eventData);
-            console.log(eventData.querySelectorAll('h3'));
+            console.log(eventData.querySelector('h3'));
+            console.log(eventData.children);
             // const children = eventData.children; // Get all children elements of the event card
             // console.log(child);
             // for (let i = 0; i < children.length; i++) {
