dd8c47c0d099aac650a6e32bfd39d31d5e428a4f
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-14 16:57:05 -0400

    Add files via upload

--- Signed-In/script.js
+++ Signed-In/script.js
@@ -144,11 +144,12 @@ document.addEventListener("DOMContentLoaded", function() {
                         const commentParagraph = document.createElement('div');
                         const commentContent = `${commentUser.textContent} - ${commentText.textContent}`;
                         commentParagraph.textContent = commentContent;
+                        commentParagraph.appendChild(deleteButton);
     
                         // Append the paragraph to the comments container
                         commentsContainer.appendChild(commentParagraph);
-                        commentsContainer.appendChild(deleteButton);
-                        
+                        // commentsContainer.appendChild(deleteButton);
+
                     });
                     popUp.querySelector('.commentsContainer').innerHTML = commentsContainer.innerHTML;
                     eventPopUpContainer.style.alignItems = 'center';
