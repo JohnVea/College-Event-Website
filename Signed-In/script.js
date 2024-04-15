cff0ed7d2b1b59e2e1ecfa9eb71cd234814832dc
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-15 17:46:06 -0400

    Add files via upload

--- Signed-In/script.js
+++ Signed-In/script.js
@@ -50,11 +50,7 @@ function closeEventPopUp() {
     eventPopUpContainer.style.display = 'none';
     eventCard.style.display = 'block';
 }
-document.addEventListener("DOMContentLoaded", function() {
-    const editCommentContainer = document.getElementById("EditCommentContainer");
-    editCommentContainer.style.display = 'none';
 
-});
 
 
 
@@ -213,21 +209,23 @@ document.addEventListener("DOMContentLoaded", function() {
                             const submitComment = document.getElementById("SubmitEditComment");
                             const CommentText = document.getElementById("CommentEditText");
                             CommentText.value = commentText1; 
-                            editCommentContainer.style.display = 'block';
-                            // eventCardContainer.style.display = 'block';
-                            // eventPopUpContainer.style.display = 'none';
-                            // window.location.reload();
-                            submitComment.addEventListener('click', async function(){
-                                await EditComment(commentText1, CommentText.value);
-                                CommentText.value = '';
-                                fetchComments();
-                                editCommentContainer.style.display = 'none';
-                            });
+                            document.addEventListener("DOMContentLoaded", function() {
+                                editCommentContainer.style.display = 'block';
+                                // eventCardContainer.style.display = 'block';
+                                // eventPopUpContainer.style.display = 'none';
+                                // window.location.reload();
+                                submitComment.addEventListener('click', async function(){
+                                    await EditComment(commentText1, CommentText.value);
+                                    CommentText.value = '';
+                                    fetchComments();
+                                    editCommentContainer.style.display = 'none';
+                                });
 
-                            const closeButton = document.getElementById("CancelEditComment");
-                            closeButton.addEventListener("click", function() {
-                                CommentText.value = '';
-                                editCommentContainer.style.display = "none";
+                                const closeButton = document.getElementById("CancelEditComment");
+                                closeButton.addEventListener("click", function() {
+                                    CommentText.value = '';
+                                    editCommentContainer.style.display = "none";
+                                });
                             });
                         });
                     });
