b71d7d68b3b21f1bdd62f726273eaf966dbaa457
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-04-16 21:26:09 -0400

    Create GetAllOrganizations.php

---
+++ LAMPAPI/GetAllOrganizations.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+$conn = new mysqli("localhost", "JohnVea", "1loveComputers", "COP4710");
+
+if ($conn->connect_error) {
+    returnWithError("Database connection error: " . $conn->connect_error);
+} else {
+    // Retrieve all RSOs from the database
+    $result = $conn->query("SELECT * FROM RSOs");
+
+    if ($result->num_rows > 0) {
+        $rsoArray = array();
+
+        // Fetch each row as an associative array and add it to the result array
+        while ($row = $result->fetch_assoc()) {
+            $rsoArray[] = $row;
+        }
+
+        // Return the RSOs as JSON
+        sendResultInfoAsJson($rsoArray);
+    } else {
+        // No RSOs found
+        returnWithError("No RSOs found");
+    }
+
+    $conn->close();
+}
+
+function sendResultInfoAsJson($obj)
+{
+    echo json_encode($obj);
+}
+
+function returnWithError($err)
+{
+    $response = array("error" => $err);
+    sendResultInfoAsJson($response);
+    http_response_code(500);
+    exit;
+}
+?>
